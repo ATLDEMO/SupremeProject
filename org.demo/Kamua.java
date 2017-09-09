a96d3a04fe66846ae81a1a6b4aac5d1431e4d6d8
maniya189 <manisht189@gmail.com>
2017-09-09 23:40:51 +0530

    this is second change

---
+++ org.demo/Kamua.java
@@ -0,0 +1,14 @@
+package org.demo;
+
+public class Kamua {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is demo project1");
+		System.out.println("this is demo project1 for second user");
+		
+
+	}
+
+}
