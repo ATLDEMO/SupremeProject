60dbd064060aa8e00df4c356a9438421cc3099e4
maniya189 <manisht189@gmail.com>
2017-09-09 23:31:11 +0530

    this is demo project Added in the repository.

---
+++ org.demo/src/test/java/org/demo/Kamua.java
@@ -0,0 +1,12 @@
+package org.demo;
+
+public class Kamua {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is demo project1");
+
+	}
+
+}
